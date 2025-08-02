924108b4a4e9013be1bbd7ab54f4c9ebe2408899
Aaron Andrade <aaron.andrade.cs@gmail.com>
2025-08-01 20:39:54 -0600

    loaderFixed

--- components/page-loader.tsx
+++ components/page-loader.tsx
@@ -207,7 +207,7 @@ export default function PageLoader() {
               transition={{ duration: 1, delay: 0.5 }}
               className="mb-8"
             >
-              <img src="/logoBlack.svg" alt="La Torre Grupo Constructora" className="h-36 w-auto" />
+              <img src="/LogoBlack.svg" alt="La Torre Grupo Constructora" className="h-36 w-auto" />
             </motion.div>
 
             {/* Loading bar */}
